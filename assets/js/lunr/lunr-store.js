7daf5567950ecc524a0dfb2801fa550530643c43
lanedenson <lanedenson@users.noreply.github.com>
2024-09-25 11:45:00 +0000

    deploy: b5a70863137d6e35082d3b75de135318e8344ccb

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Getting Started with GitHub Pages",
+        "excerpt":"Your content goes here. You can use Markdown formatting.   Introduction   GitHub Pages is a great way to host static websites directly from your GitHub repository.   Key Features      Free hosting   Automatic builds with Jekyll   Custom domain support   Getting Started   To get started with GitHub Pages, you need to…  ","categories": ["web-development"],
+        "tags": ["github","jekyll","static-site"],
+        "url": "/web-development/2024/03/19/getting-started-with-github-pages.html",
+        "teaser": null
+      },{
         "title": "Welcome to Jekyll! 2",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
         "tags": ["Jekyll","update"],
